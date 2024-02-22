201561c54534f787a65ba5958b07a3869ad5dc84
casscannelle <cassia.maffei@gmail.com>
2024-02-22 16:48:35 -0300

    Componente busca na API

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import CarForm from './components/Carro/Carro';
-import CarInfo from './components/Resultado/Resultado';
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import BuscaFIPE from './components/BuscaFIPE/BuscaFIPE';
 
-const App = () => {
-  const [carInfo, setCarInfo] = useState(null);
-
-  const searchCar = ({ ano, marca, modelo }) => {
-    axios.get(`https://parallelum.com.br/fipe/api/${marca}/${modelo}/${ano}.json`)
-      .then(response => {
-        setCarInfo(response.data);
-      })
-      .catch(error => {
-        console.error('Erro ao buscar informações do carro:', error);
-        setCarInfo(null);
-      });
-  };
-
+function App() {
+  
   return (
-    <div>
-      <h1>Busca na Tabela FIPE</h1>
-      <CarForm onSearch={searchCar} />
-      {carInfo && <CarInfo carInfo={carInfo} />}
+    <div className="App">
+      <BuscaFIPE/>
     </div>
   );
-};
+}
 
 export default App;
